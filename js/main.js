6f2b43f9c7013d82cef1c916f192d06105570427
Rogulenko-top <69743527+Rogulenko-top@users.noreply.github.com>
2022-03-24 17:22:59 +0500

    9.6. Помощь друга

--- js/main.js
+++ js/main.js
@@ -3,3 +3,5 @@ import './data.js';
 import './picture.js';
 import './big-picture.js';
 import './processing-picture.js';
+import './scale-image.js';
+import './filter-image.js';
---
+++ js/scale-image.js
@@ -0,0 +1,27 @@
+import {SCALE_STEP} from './const.js';
+import {DECIMAL_SYSTEM, DIVISION_ONE_HUNDRED, MAX_PERCENTAGE_VALUE, MIN_PERCENTAGE_VALUE} from './const.js';
+
+const imagePreviewElement = document.querySelector('.img-upload__preview img');
+
+const rangeInput = document.querySelector('.scale__control--value');
+
+const buttonRangeBigger = document.querySelector('.scale__control--bigger');
+const buttonRangeSmaller = document.querySelector('.scale__control--smaller');
+
+buttonRangeBigger.addEventListener('click', () => {
+  let scale = parseInt(rangeInput.value, DECIMAL_SYSTEM);
+  if(!(scale > MAX_PERCENTAGE_VALUE)){
+    scale = scale + SCALE_STEP;
+    rangeInput.value = `${scale}%`;
+    imagePreviewElement.style.transform = `scale(${scale / DIVISION_ONE_HUNDRED})`;
+  }
+});
+
+buttonRangeSmaller.addEventListener('click', () => {
+  let scale = parseInt(rangeInput.value, DECIMAL_SYSTEM);
+  if(!(scale <= MIN_PERCENTAGE_VALUE)){
+    scale = scale - SCALE_STEP;
+    rangeInput.value = `${scale}%`;
+    imagePreviewElement.style.transform = `scale(${scale / DIVISION_ONE_HUNDRED})`;
+  }
+});
